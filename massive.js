6068eb031bc54683ed192163992166cbcd857de4
Djudio <78900962+Djudio@users.noreply.github.com>
2021-03-23 15:07:31 +0200

    Add files via upload

---
+++ massive.js
@@ -0,0 +1,68 @@
+// //task 1 
+// let textMassive = ["Привет ", "мир ", " !"]
+// let text = textMassive[0] + textMassive[1] + textMassive[2]
+// alert(text)
+
+//task 2
+// let helloWorld =["Привет ", "мир ", " !"]
+// alert(helloWorld [0] = "Пока " + helloWorld [1] + helloWorld [2])
+
+
+//task 3
+// let arr = ["1","2","3","4","5"]
+// alert(arr)
+
+// let arr = ["1","2","3","4","5"]
+// alert(arr[0] + arr[1] + arr[2] + arr[3] + arr[4])
+
+//task 4 
+// let arr =  ["a", "b", "c"]
+// console.log(arr);
+
+//task 5 
+// let arr = ["a", "b", "c"]
+// alert(arr [0]) 
+// alert(arr [1]) 
+// alert(arr [2]) 
+
+// task 6 
+// let arr = ["a", "b", "c", 'd']
+// alert(arr[0] + "+" + arr[1] + ", " +  arr[2] + "+" + arr[3])
+
+// // Task 7 
+// let arr =["2","5","3","9"]
+// let firstStep = arr[0] * arr[1]
+// let secondStep= arr[2] * arr[3]
+// let result=firstStep+secondStep
+// alert(result)
+
+// Task 8 
+// let arr = [
+//     ["1","2","3"],
+//     ["4","5","6"],
+//     ["7","8","9"]
+// ]
+
+// alert(arr[1][0])
+
+// Task 9 
+let arr = [
+    ["1","2","3"],
+    ["4","5","6"],
+    ["7","8","9"]
+]
+
+// alert(arr[0][1] + arr[1][0] + arr[1][2]+ arr[2][1]) //но если матрица большая то так не сделашешь
+
+for (let index = 0; index < arr.length; index++) {
+    alert(arr);
+}
+
+//index=0 - переменная (счетчик) начинаем с 0
+//index/length - размер не больше длины массива 
+//index++ - ? 
+
+
+
+
+
